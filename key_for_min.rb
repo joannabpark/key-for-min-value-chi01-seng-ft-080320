3ce963f7ae93fc0703b7880cde0bda4f82ebcb0e
JoAnna Park <joannabpark1@gmail.com>
2020-07-21 13:21:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,6 @@ def key_for_min_value(name_hash)
   key_smallest_value = nil
 name_hash.each do |key, value|
   if smallest_value == nil
-    smallest_value = key
-    
+    smallest_value = value
+    key_smallest_value = key    
 end
